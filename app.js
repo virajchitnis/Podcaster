8a8666570ba7e8d3e48404de35b97498e1b64cd6
Viraj Chitnis <chitnisviraj@gmail.com>
2020-07-23 16:47:01 +0100

    Generate feeds based on configuration

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const xml = require('xml');
 const fs = require('fs');
 const path = require('path');
 
-const server_conf = require('./config/server.json');
+const config_dir = 'config';
+const server_conf = require('./' + path.join(config_dir, 'server.json'));
 const app = express();
 app.use(morgan(server_conf.logStyle));
 app.use(express.static('public'));
@@ -17,6 +18,39 @@ app.use(express.static('public'));
  */
 app.get('/health', (req, res) => {
     res.send('Up!');
+});
+
+// Build list of podcasts from configuration
+const podcasts_dirs = fs.readdirSync(config_dir)
+.filter(name => fs.lstatSync(path.join(config_dir, name)).isDirectory())
+.map(name => './' + path.join(config_dir, name));
+
+var podcasts = [];
+podcasts_dirs.forEach((dir) => {
+    var podcast_episodes = fs.readdirSync(dir)
+    .filter(name => path.extname(name) === '.json' && name !== "info.json")
+    .map(name => require('./' + path.join(dir, name)));
+
+    podcasts.push({
+        directory: dir,
+        info: require('./' + path.join(dir, 'info.json')),
+        episodes: podcast_episodes
+    });
+});
+
+// Build endpoints for each podcast
+podcasts.forEach((show) => {
+    var feed = new podcast(show.info);
+    show.episodes.forEach((episode) => {
+        episode.enclosure.url = `${server_conf.websiteRoot}${episode.enclosure.url}`;
+        feed.addItem(episode);
+    });
+    show.feedXML = feed.buildXml();
+
+    app.get(`/${show.info.shortName}/feed.xml`, (req, res) => {
+        res.set('Content-Type', 'text/xml');
+        res.send(show.feedXML);
+    });
 });
 
 app.listen(server_conf.port, () => console.log(`Podcaster listening at http://localhost:${server_conf.port}`));
