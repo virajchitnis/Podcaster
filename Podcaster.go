820cff4d75ec5df0edaa3961f7ee7317a1f1af7f
Viraj Chitnis <chitnisviraj@gmail.com>
2020-07-28 12:45:09 +0100

    Added an endpoint

--- Podcaster.go
+++ Podcaster.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	fmt.Println("Welcome to Podcaster!")
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Welcome to Podcaster!",
+		})
+	})
+	r.Run()
 }
